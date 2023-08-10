ca4f1730da3e3c3c83881bc2814906f061984d9f
baoyi <60701681+BaoYiLeeby@users.noreply.github.com>
2023-08-11 00:26:00 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using CDN_WebApplication1.UserModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CDN_WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<PeopleContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("PeopleContext")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
